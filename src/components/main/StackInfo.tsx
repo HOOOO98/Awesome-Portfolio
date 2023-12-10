8e1eb09e838a4d047d3f00c77f79de4d1d62893a
HOOOO98 <loading2us@gmail.com>
2023-12-10 13:32:29 +0900

    Feat: apply responsive design

--- src/components/main/StackInfo.tsx
+++ src/components/main/StackInfo.tsx
@@ -33,23 +33,25 @@ function StackInfo() {
         <motion.div
           ref={react}
           style={{ rotate: isReactView ? smoothRotation : 'none' }}
-          className='w-[250px] h-[250px]'
+          className='w-[250px] h-[250px] mb:w-[150px] mb:h-[150px]'
         >
           <Image
             src='/images/reactIcon.png'
             alt='React Icon'
             width={250}
             height={250}
-            className='w-[250px] h-[250px]'
+            className='w-[250px] h-[250px] mb:w-[150px] mb:h-[150px]'
           />
         </motion.div>
-        <div className='w-[300px]'>
-          <h1 className='text-6xl font-bold leading-normal'>React</h1>
-          <h2 className='text-gray-500 text-xl leading-normal'>
+        <div className='w-[300px] mb:w-[170px]'>
+          <h1 className='text-6xl font-bold leading-normal mb:text-2xl'>
+            React
+          </h1>
+          <h2 className='text-gray-500 text-xl leading-normal mb:text-sm'>
             Utilizing the features of the virtual DOM, I harness the benefits of
             efficient rendering.
           </h2>
-          <h2 className='text-gray-500 text-xl leading-normal'>
+          <h2 className='text-gray-500 text-xl leading-normal mb:text-sm'>
             Additionally, leveraging the extensive ecosystem, I am able to
             extend projects seamlessly.
           </h2>
@@ -59,19 +61,21 @@ function StackInfo() {
         <motion.div
           ref={next}
           animate={{ scale: isNextView ? [0, 0.5, 1] : 'none' }}
-          className='w-[250px] h-[250px]'
+          className='w-[250px] h-[250px] mb:w-[200px] mb:h-[200px]'
         >
           <Image
             src='/images/nextIcon.png'
             alt='Next Icon'
             width={250}
             height={250}
-            className='w-[250px] h-[250px]'
+            className='w-[250px] h-[250px] mb:w-[200px] mb:h-[200px]'
           />
         </motion.div>
-        <div className='w-[300px]'>
-          <h1 className='text-6xl font-bold leading-normal'>Next</h1>
-          <h2 className='text-gray-500 text-xl leading-normal'>
+        <div className='w-[300px] text-center'>
+          <h1 className='text-6xl font-bold leading-normal mb:text-4xl'>
+            Next
+          </h1>
+          <h2 className='text-gray-500 text-xl leading-normal mb:text-lg'>
             Dynamic routing allows for straightforward implementation of pages,
             and it enables the differentiation and utilization of server
             components and client components.
@@ -82,25 +86,27 @@ function StackInfo() {
         <motion.div
           ref={typescript}
           animate={{
-            translateX: isTypescriptView ? ['-400px', '10px', '0px'] : 'none',
+            translateX: isTypescriptView ? ['-100px', '10px', '0px'] : 'none',
           }}
-          className='w-[250px] h-[250px]'
+          className='w-[250px] h-[250px] mb:w-[100px] mb:h-[100px]'
         >
           <Image
             src='/images/typescriptIcon.png'
             alt='Typescript Icon'
             width={250}
             height={250}
-            className='w-[250px] h-[250px]'
+            className='w-[250px] h-[250px]  mb:w-[100px] mb:h-[100px]'
           />
         </motion.div>
-        <div className='w-[300px]'>
-          <h1 className='text-6xl font-bold leading-normal'>Typescript</h1>
-          <h2 className='text-gray-500 text-xl leading-normal'>
+        <div className='w-[300px] mb:w-[170px]'>
+          <h1 className='text-6xl font-bold leading-normal mb:text-2xl'>
+            Typescript
+          </h1>
+          <h2 className='text-gray-500 text-xl leading-normal mb:text-sm'>
             Enhancing clarity in API communication, I've reduced errors by
             enforcing precise data type specifications.
           </h2>
-          <h2 className='text-gray-500 text-xl leading-normal'>
+          <h2 className='text-gray-500 text-xl leading-normal mb:text-sm'>
             Additionally, I've improved transparency by explicitly declaring
             data types for data transfers between components.
           </h2>
@@ -110,21 +116,23 @@ function StackInfo() {
         <motion.div
           ref={reactnative}
           animate={{
-            translateX: isReactnativeView ? ['-400px', '10px', '0px'] : 'none',
+            translateX: isReactnativeView ? ['-100px', '10px', '0px'] : 'none',
           }}
-          className='w-[250px] h-[250px]'
+          className='w-[250px] h-[250px] mb:w-[80px] mb:h-[80px]'
         >
           <Image
             src='/images/androidIcon.png'
             alt='Android Icon'
             width={250}
             height={250}
-            className='w-[250px] h-[250px]'
+            className='w-[250px] h-[250px] mb:w-[80px] mb:h-[80px]'
           />
         </motion.div>
-        <div className='w-[300px] '>
-          <h1 className='text-6xl font-bold leading-normal'>React Native</h1>
-          <h2 className='text-gray-500 text-xl leading-normal'>
+        <div className='w-[300px] mb:w-[160px]'>
+          <h1 className='text-6xl font-bold leading-normal mb:text-2xl'>
+            React Native
+          </h1>
+          <h2 className='text-gray-500 text-xl leading-normal mb:text-sm'>
             Utilizing React syntax, it's possible to extend the service into a
             mobile app.
           </h2>
@@ -132,23 +140,25 @@ function StackInfo() {
         <motion.div
           ref={reactnative}
           animate={{
-            translateX: isReactnativeView ? ['400px', '-10px', '0px'] : 'none',
+            translateX: isReactnativeView ? ['100px', '-10px', '0px'] : 'none',
           }}
-          className='w-[250px] h-[250px]'
+          className='w-[250px] h-[250px] mb:w-[80px] mb:h-[80px]'
         >
           <Image
             src='/images/swiftIcon.png'
             alt='Android Icon'
             width={250}
             height={250}
-            className='w-[250px] h-[250px]'
+            className='w-[250px] h-[250px] mb:w-[80px] mb:h-[80px]'
           />
         </motion.div>
       </div>
       <div className='w-screen h-screen flex flex-row justify-evenly items-center'>
-        <div className='w-[300px]'>
-          <h1 className='text-6xl font-bold leading-normal'>Python</h1>
-          <h2 className='text-gray-500 text-xl leading-normal'>
+        <div className='w-[300px] mb:w-[170px]'>
+          <h1 className='text-6xl font-bold leading-normal  mb:text-2xl'>
+            Python
+          </h1>
+          <h2 className='text-gray-500 text-xl leading-normal  mb:text-sm'>
             I can quickly approach solutions to algorithmic problems by
             leveraging Python libraries.
           </h2>
@@ -156,16 +166,16 @@ function StackInfo() {
         <motion.div
           ref={python}
           animate={{
-            translateX: isPythonView ? ['200px', '-10px', '0px'] : 'none',
+            translateX: isPythonView ? ['100px', '-10px', '0px'] : 'none',
           }}
-          className='w-[250px] h-[250px]'
+          className='w-[250px] h-[250px] mb:w-[100px] mb:h-[100px]'
         >
           <Image
             src='/images/pythonIcon.png'
             alt='React Icon'
             width={250}
             height={250}
-            className='w-[250px] h-[250px]'
+            className='w-[250px] h-[250px] mb:w-[100px] mb:h-[100px]'
           />
         </motion.div>
       </div>
@@ -175,19 +185,21 @@ function StackInfo() {
           animate={{
             translateY: isCypressView ? ['-200px', '10px', '0px'] : 'none',
           }}
-          className='w-[250px] h-[250px]'
+          className='w-[250px] h-[250px]  mb:w-[150px] mb:h-[150px]'
         >
           <Image
             src='/images/cypressIcon.png'
             alt='Cypress Icon'
             width={250}
             height={250}
-            className='w-[250px] h-[250px]'
+            className='w-[250px] h-[250px]  mb:w-[150px] mb:h-[150px]'
           />
         </motion.div>
-        <div className='w-[300px]'>
-          <h1 className='text-6xl font-bold leading-normal'>Cypress</h1>
-          <h2 className='text-gray-500 text-xl leading-normal'>
+        <div className='w-[300px] text-center'>
+          <h1 className='text-6xl font-bold leading-normal mb:text-4xl'>
+            Cypress
+          </h1>
+          <h2 className='text-gray-500 text-xl leading-normal mb:text-lg'>
             Using Cypress, I can conduct precise verification of each function
             and feature through testing.
           </h2>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,12 @@ const config: Config = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  theme: {
+    screens: {
+      mb: { min: '320px', max: '767px' },
+      tb: { min: '768px', max: '1023px' },
+    },
+  },
   plugins: [],
 };
 export default config;
